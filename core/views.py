74b524aacaac959f87b587ba8cd6d5303ac0a5db
Lukasz Malucha <lucasmalucha@gmail.com>
2020-07-30 10:12:25 +0100

    model

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from core.models import NewsItem
 
-# Create your views here.
+class NewsItemListView(generic.ListView):
+    model = NewsItem
+    template_name = "news_item_list.html"
